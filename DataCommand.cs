090f7622104a34b35d27daf06ff973acd9000329
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-02-11 15:00:40 +0100

    Add files via upload

---
+++ DataCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIP
+{
+    public abstract class DataCommand
+    {
+        public string Output { get;  set; }
+        public bool Exited { get; set; }
+        
+        public string _promptString { get; set; }
+
+        public void ExecuteCommand(Command command)
+        {
+            try
+            {
+                command.Launch();
+                if (Output.Length > 0)
+                {
+                    Console.WriteLine(Output);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                Console.Error.WriteLine("{0}: path not found", command);
+            }
+        }
+
+        public string Prompt()
+        {
+            Console.Write(_promptString);
+            string userCommand = Console.ReadLine();
+            return userCommand;
+        }
+
+        public Command PromptCommand()
+        {
+            string commandLine = Prompt();
+            return new Command(commandLine);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.IO;
+
+namespace DIP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataCommand datacommand = new Terminal();
+            while (!datacommand.Exited)
+            {
+                Command command = datacommand.PromptCommand();
+                datacommand.ExecuteCommand(command);
+            }
+        }
+    }
+
+  
+
+    public class Terminal : DataCommand
+    {
+
+
+        public Terminal()
+        {
+            _promptString = String.Format("{0}$ ", System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            Exited = false;
+        }
+    }
+
+    public class Command : DataCommand
+    {
+        public string Executable { get; private set; }
+        public string[] Arguments { get; private set; }
+
+
+        public Command(string line)
+        {
+            string[] splittedLine = line.Split(' ');
+            Executable = splittedLine[0];
+            Arguments = splittedLine.Skip(1).ToArray();
+            Output = "";
+        }
+
+        public override string ToString()
+        {
+            if (ExecutableFullPath is null)
+            {
+                return Executable;
+            }
+            else
+            {
+                return ExecutableFullPath;
+            }
+        }
+
+        public void Launch()
+        {
+            string commandOutput;
+            if (Executable.Length == 0)
+            {
+                return;
+            }
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = ExecutableFullPath;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.Arguments = String.Join(' ', Arguments);
+                process.Start();
+
+                commandOutput = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+            }
+            Output = commandOutput;
+        }
+
+        public string ExecutableFullPath
+        {
+            get
+            {
+                string executableWithExtension;
+                if (Executable.EndsWith(".exe"))
+                {
+                    executableWithExtension = Executable;
+                }
+                else
+                {
+                    executableWithExtension = Executable + ".exe";
+                }
+                if (File.Exists(executableWithExtension))
+                {
+                    return Path.GetFullPath(Executable);
+                }
+
+                string values = Environment.GetEnvironmentVariable("PATH");
+                foreach (var path in values.Split(Path.PathSeparator))
+                {
+                    string fullPath = Path.Combine(path, executableWithExtension);
+                    if (File.Exists(fullPath))
+                    {
+                        return fullPath;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
